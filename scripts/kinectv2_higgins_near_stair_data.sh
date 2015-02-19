0eb5f7319ce608f44758f4c4469c834bad07dec8
Mitchell Wills <mwills@wpi.edu>
2015-02-19 03:36:41 -0500

    Updated stair descriptions to have number of stairs Also added some new scripts for publishing smaller sets of test data

---
+++ scripts/kinectv2_higgins_near_stair_data.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+find `catkin_find walrus_testdata kinectv2/stairs/higgins_stair_near` -name *.pcd -print0 | xargs -0 rosrun walrus_testdata multi_pcd_pub /cloud_pcd:=/walrus/boom/kinect/depth/points
