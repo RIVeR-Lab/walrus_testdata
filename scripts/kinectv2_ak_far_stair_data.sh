704c96431b3df212e9047adb5192c4d094f5b0e4
Mitchell Wills <mwills@wpi.edu>
2015-03-06 19:15:15 -0500

    Removed /walrus namespace from test scripts

--- scripts/kinectv2_ak_far_stair_data.sh
+++ scripts/kinectv2_ak_far_stair_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find `catkin_find walrus_testdata kinectv2/stairs/ak_far` -name '*.pcd' -print0 | xargs -0 rosrun walrus_testdata multi_pcd_pub /cloud_pcd:=/walrus/boom/kinect/depth/points
+find `catkin_find walrus_testdata kinectv2/stairs/ak_far` -name '*.pcd' -print0 | xargs -0 rosrun walrus_testdata multi_pcd_pub /cloud_pcd:=/boom/kinect/depth/points
--- scripts/kinectv2_higgins_short_stair_data.sh
+++ scripts/kinectv2_higgins_short_stair_data.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find `catkin_find walrus_testdata kinectv2/stairs/higgins_short` -name '*.pcd' -print0 | xargs -0 rosrun walrus_testdata multi_pcd_pub /cloud_pcd:=/walrus/boom/kinect/depth/points
+find `catkin_find walrus_testdata kinectv2/stairs/higgins_short` -name '*.pcd' -print0 | xargs -0 rosrun walrus_testdata multi_pcd_pub /cloud_pcd:=/boom/kinect/depth/points
